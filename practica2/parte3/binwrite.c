670e59189008cfdcb4eb2938181c1816f4deaacb
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-10-05 19:27:27 +0200

    plan de accion porque me estoy agobiando mucho mucho asies

--- practica2/parte3/binwrite.c
+++ practica2/parte3/binwrite.c
@@ -7,13 +7,69 @@
 /*
 
 TODO:
+
 -c
 
 
+limpia lo que hubiera en el fichero
+por cada entrada de alumno:
+    parsea el input
+    lo añade al array de la db del programa
+
+escribe al archivo:
+    el numero de entradas
+    todas las entradas
+
+
 -l
+
+
+parsea el archivo y puebla la db
+por cada entrada de la db:
+    imprime con formato cada entrada al output
+
 -h
+
+imprime el uso del programa
+
+
 -a
+
+por cada entrada de alumno:
+    parsea el input
+    busca si existe en la db
+    si no: añade a la db
+
+
+
 -q
+
+dependiendo del param:
+    -i
+        busca si existe alguien con id = param
+        si existe: printea esa cosa
+    -n
+        busca si existe alguien con nif = param
+        si existe: printea esa cosa
+
+
+independientemente de lo que tenga que hacer:
+-f
+
+abre el archivo optarg
+parsea las nelems entradas a db.
+
+(hace lo que tenga que hacer)
+
+reescribe las entradas de db en el archivo
+cierra el archivo
+*/
+
+/*
+PREGUNTAS AL POFE
+
+student_t es un puntero opaco al struct, cómo sacamos su tamaño?
+
 
 */
 
@@ -41,10 +97,14 @@ int dump_entries(student_t* entries, int nr_entries, FILE* students)
 
 }
 
+
+
 student_t* read_student_file(FILE* students, int* nr_entries) 
 {
 
 }
+
+
 
 char* loadstr(FILE* students)
 {
