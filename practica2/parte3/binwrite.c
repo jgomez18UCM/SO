032fcd8064d2cc1a932dbdf9a25172f48ffb9553
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-10-05 18:01:56 +0200

    Update binwrite.c

--- practica2/parte3/binwrite.c
+++ practica2/parte3/binwrite.c
@@ -1,3 +1,16 @@
+#define MAX_CHARS_NIF  9
+
+typedef struct {
+    int student_id; 
+    char NIF[MAX_CHARS_NIF+1];  
+    char* first_name;
+    char* last_name;
+} student_t;
+
+
+
+
+
 int main(void){
     return 0;
 }
