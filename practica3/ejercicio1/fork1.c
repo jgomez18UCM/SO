098466bd72a16fde1f848c2529913b7fac3ff3c7
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-10-19 19:17:33 +0200

    1 y 2 de p3

--- practica3/ejercicio1/fork1.c
+++ practica3/ejercicio1/fork1.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /*programa que crea dos hijos: uno no cambia de ejecutable y otro si */
 
 
 int main(int argc, char **argv)
 {
+
+	
+
+	pid_t pid = fork();
+
+	//si son padre o hijo escriben que lo son + su ID y el de su padre
+
+	if(pid == 0)
+	{
+		printf("Soy el proceso HIJO de ID %d, mi padre tiene ID %d\n", getpid(), getppid());	
+	}
+	else
+	{
+		printf("Soy el proceso PADRE de ID %d, mi padre tiene ID %d\n", getpid(), getppid());
+	}
+
+	
+	pid_t pid2 = fork();
+
+	if(pid2 == 0)
+	{
+		printf("Soy el proceso HIJO de ID %d, mi padre tiene ID %d\n", getpid(), getppid());	
+		execvp(argv[1], argv+1);
+	}
+	else
+	{
+		printf("Soy el proceso PADRE de ID %d, mi padre es %d\n", pid2, getppid());
+	}
+
+	while (waitpid(pid2, NULL, 0) != -1);
+	if (pid != 0) while (waitpid(pid, NULL, 0) != -1);
+	
 
 	return 0;
 }
--- practica3/ejercicio2/fork5.c
+++ practica3/ejercicio2/fork5.c
@@ -10,6 +10,73 @@
 
 int main(int argc, char **argv)
 {
+	pid_t pid1 = fork();
+
+	if(pid1 == 0) //POV: eres p1
+	{
+		execlp("echo", "echo", "P1", NULL);
+	}
+
+	while(waitpid(pid1, NULL, 0) != -1);
+
+	pid_t pid2 = fork();
+
+	if(pid2 == 0) //POV: eres p2
+	{
+		execl("/usr/bin/echo", "/usr/bin/echo", "P2", NULL);
+	}
+
+	pid_t pid5 = fork();
+
+	if(pid5 == 0) //POV: eres p5
+	{
+		execlp("echo", "echo", "P5", NULL);
+	}
+
+	pid_t pid7 = fork();
+
+	if(pid7 == 0) //POV: eres p7
+	{
+		execlp("echo", "echo", "P7", NULL);
+	}
+
+	while(waitpid(pid2, NULL, 0) != -1);
+
+	// 3 y 4
+
+	pid_t pid3 = fork();
+
+	if(pid3 == 0) //POV: eres p3
+	{
+		execlp("echo", "echo", "P3", NULL);
+	}
+
+	pid_t pid4 = fork();
+
+	if(pid4 == 0) //POV: eres p4
+	{
+		execl("/usr/bin/echo", "/usr/bin/echo", "P4", NULL);
+	}
+
+	while (waitpid(pid4, NULL, 0) != -1 && waitpid(pid5, NULL, 0) != -1);
+
+	pid_t pid6 = fork();
+
+	if(pid6 == 0) //POV: eres p6
+	{
+		execl("/usr/bin/echo", "/usr/bin/echo", "P6", NULL);
+	}
+
+	while(waitpid(pid4, NULL, 0) != -1 && waitpid(pid6, NULL, 0) != -1 && waitpid(pid7, NULL, 0) != -1);
+
+	pid_t pid8 = fork();
+
+	if(pid8 == 0) //POV: eres p8
+	{
+		execl("/usr/bin/echo", "/usr/bin/echo", "P8", NULL);
+	}
+
+	while(waitpid(pid8, NULL, 0) != -1);
 
 	return 0;
 }
