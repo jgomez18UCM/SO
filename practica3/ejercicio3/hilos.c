65012e831409223c1806e22c4990cfb0fc63bb6b
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-10-20 12:43:43 +0200

    lo de clase

--- practica3/ejercicio3/hilos.c
+++ practica3/ejercicio3/hilos.c
@@ -1,13 +1,63 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
+#define NUM_HILOS 5
+typedef struct 
+{
+	int t_ID;
+	char t_TYPE;
+}thread_data;
+
 
 void *thread_usuario(void *arg)
 {
+	thread_data * copia = (thread_data *) arg;
 
+	int copiaTID = copia->t_ID;
+	char copiaTTYPE = copia->t_TYPE;
+
+	free(arg);
+
+	pthread_t self = pthread_self();
+
+	printf("Hola, soy el proceso %ld, nº %d y soy", self, copiaTID);
+	if(copiaTTYPE == 'P'){printf(" prioritario\n");}
+	else if(copiaTTYPE == 'N'){printf(" no prioritario\n");}
+	else printf(" un error\n");
 }
 
 int main(int argc, char* argv[])
 {
+
+		pthread_t threads [NUM_HILOS];
+	for (int i = 0; i < (NUM_HILOS); i++)
+	{
+		 thread_data * data = malloc(sizeof(thread_data));
+		data->t_ID = i;
+		if(i % 2 == 0)
+		{//es par
+			data->t_TYPE = 'P';
+		}
+		else
+		{//es parn't 	
+			data->t_TYPE = 'N';
+		}
+		
+		pthread_attr_t attr;
+		pthread_create(&threads[i], NULL, thread_usuario, data);
+
+	}
+	
+		for (int i = 0; i < NUM_HILOS; i++)
+		{
+		pthread_join(threads[i], NULL);
+			/* code */
+		}
+		
+
+
+
 
 	return 0;
 }
--- practica3/ejercicio4/signalsyfork.c
+++ practica3/ejercicio4/signalsyfork.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
+pid_t pid;
+
+void foo(int sig)
+{
+	kill(pid, SIGALRM);
+}
 
 /*programa que temporiza la ejecución de un proceso hijo */
 
 
 int main(int argc, char **argv)
 {
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+		execvp(argv[1], argv + 1);
+	}
+	else
+	{
+		//crea la señal
+		struct sigaction accion;
+		accion.sa_handler = &foo;
+		sigaction(SIGALRM, &accion, NULL);
+		
+		
+		alarm(5);
+	}
+	int status = 0;
+
+	while(wait(&status) != pid); //hace rla cosa de tal
+
+	//hacer cosas con status
 
 	return 0;
 }
