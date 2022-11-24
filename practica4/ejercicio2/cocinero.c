b952bb526753c174f762d2f31184825959fe8141
RuBeOX49 <daniel.alonsoherranz@gmail.com>
2022-11-24 12:10:32 +0100

    Parte2

--- practica4/ejercicio2/cocinero.c
+++ practica4/ejercicio2/cocinero.c
@@ -1,19 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+
 
 #define M 10
 
 int finish = 0;
 
-void putServingsInPot(int servings)
+sem_t *salvajes_q;
+
+sem_t *cocinero;
+sem_t *salvaje;
+
+
+void putServingsInPot(int servings, int * caldero)
 {
+	sem_wait(&cocinero);
+	*caldero += servings;
+	sem_post(&salvaje);
 }
 
-void cook(void)
+void cook(int * caldero)
 {
 	while(!finish) {
-		putServingsInPot(M);
+		putServingsInPot(M, caldero);
 	}
 }
 
@@ -22,8 +36,41 @@ void handler(int signo)
 	finish = 1;
 }
 
+
+
 int main(int argc, char *argv[])
 {
+
+	int shd, *caldero;
+
+	shd = shm_open("/CALDERO", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+	ftruncate(shd, sizeof(int));
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_WRITE, MAP_SHARED, shd, 0);
+
+	//Modificar señales
+
+
+	//Setup semaforos
+
+	cocinero = sem_open("/COCINERO", O_CREAT, 0700, 0);
+	salvaje = sem_open("/SALVAJE", O_CREAT, 0700, 0);
+
+	salvajes_q = sem_open("/SALVAJES_Q", O_CREAT, 0700, 0);
+
+	cook(caldero);
+
+	munmap(caldero, sizeof(int));
+	shm_unlink("/CALDERO");
+	
+	sem_close(cocinero);
+	sem_close(salvaje);
+	sem_close(salvajes_q);
+
+	sem_unlink("/COCINERO");
+	sem_unlink("/SALVAJE");
+	sem_unlink("/SALVAJES_Q");
+
+
 
 	return 0;
 }
--- practica4/ejercicio2/salvajes.c
+++ practica4/ejercicio2/salvajes.c
@@ -1,12 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
 
 #define NUMITER 3
 
-int getServingsFromPot(void)
-{
+sem_t *salvajes_q;
 
+sem_t *cocinero;
+sem_t *salvaje;
+
+int getServingsFromPot(int * caldero)
+{
+	
+	sem_wait(&salvajes_q);
+
+        if(caldero == 0){
+            sem_post(&cocinero);
+            sem_wait(&salvaje);
+        }
+
+        caldero--;
+        sem_post(&salvajes_q);
+	
 }
 
 void eat(void)
@@ -16,13 +35,28 @@ void eat(void)
 	sleep(rand() % 5);
 }
 
-void savages(void)
+void savages(int * caldero)
 {
-
+	int i = 0;
+	for(i = 0; i < NUMITER; i++){
+		getServingsFromPot(caldero);
+		eat();
+	}
 }
 
 int main(int argc, char *argv[])
 {
+
+	int shd, *caldero;
+
+	shd = shm_open("/CALDERO", O_RDWR, 0);
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, shd, 0);
+
+	cocinero = sem_open("/COCINERO", 0);
+	salvaje = sem_open("/SALVAJE", 0);
+	salvajes_q = sem_open("/SALVAJES_Q", 0);
+
+	savages(caldero);
 
 	return 0;
 }
