df3d50b8ebb3eb5ced711161eff2210a1472ddca
jgomez18UCM <jgomez18@ucm.es>
2022-11-24 11:57:55 +0100

    SALVAJES HIJOS DE PUTA

--- practica4/ejercicio2/cocinero.c
+++ practica4/ejercicio2/cocinero.c
@@ -1,19 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <signal.h>
 
-#define M 10
+#define M 5
 
 int finish = 0;
 
-void putServingsInPot(int servings)
+sem_t *salvajes_q;
+
+sem_t *cocinero;
+sem_t *salvaje;
+
+
+void putServingsInPot(int servings, int * caldero)
 {
+	sem_wait(cocinero);
+	printf("Reponiendo\n");
+	*caldero += servings;
+	printf("caldero %d\n", *caldero);
+
+	sem_post(salvaje);
 }
 
-void cook(void)
+void cook(int * caldero)
 {
 	while(!finish) {
-		putServingsInPot(M);
+		putServingsInPot(M, caldero);
 	}
 }
 
@@ -22,8 +39,54 @@ void handler(int signo)
 	finish = 1;
 }
 
+
+
 int main(int argc, char *argv[])
 {
+
+	int shd, *caldero;
+
+	struct sigaction sa;
+
+	sa.sa_handler = handler;
+	sa.sa_flags = 0;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGTERM, &sa, NULL);
+	
+	
+	//signal(SIGINT, handler);
+	//signal(SIGTERM, handler);
+
+
+
+	shd = shm_open("/CALDERO", O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+	ftruncate(shd, sizeof(int));
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_WRITE | PROT_READ, MAP_SHARED, shd, 0);
+
+	//Modificar señales
+
+
+	//Setup semaforos
+
+	cocinero = sem_open("/COCINERO", O_CREAT, 0700, 0);
+	salvaje = sem_open("/SALVAJE", O_CREAT, 0700, 0);
+
+	salvajes_q = sem_open("/SALVAJES_Q", O_CREAT, 0700, 1);
+
+	cook(caldero);
+
+	munmap(caldero, sizeof(int));
+	shm_unlink("/CALDERO");
+	
+	sem_close(cocinero);
+	sem_close(salvaje);
+	sem_close(salvajes_q);
+
+	sem_unlink("/COCINERO");
+	sem_unlink("/SALVAJE");
+	sem_unlink("/SALVAJES_Q");
+
+
 
 	return 0;
 }
--- practica4/ejercicio2/salvajes.c
+++ practica4/ejercicio2/salvajes.c
@@ -1,28 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
 
 #define NUMITER 3
 
-int getServingsFromPot(void)
+sem_t *salvajes_q;
+
+sem_t *cocinero;
+sem_t *salvaje;
+
+int getServingsFromPot(int * caldero)
 {
+	sem_wait(salvajes_q);
+	if((*caldero) == 0){
+		sem_post(cocinero);
+		sem_wait(salvaje);
+	}
+
+	(*caldero)--;
+	sem_post(salvajes_q);
 
 }
 
-void eat(void)
+void eat(int i)
 {
 	unsigned long id = (unsigned long) getpid();
-	printf("Savage %lu eating\n", id);
-	sleep(rand() % 5);
+	printf("Savage %lu ate %d times\n", id, i+1);
+	sleep(rand() * id % 5);
 }
 
-void savages(void)
+void savages(int * caldero)
 {
-
+	int i = 0;
+	for(i = 0; i < NUMITER; i++){
+		getServingsFromPot(caldero);
+		eat(i);
+	}
 }
 
 int main(int argc, char *argv[])
 {
 
+	int shd, *caldero;
+
+	if((shd = shm_open("/CALDERO", O_RDWR, 0)) == -1){
+		printf("No se crearon los recursos compartidos: CALDERO");
+		exit(EXIT_FAILURE);
+	}
+	caldero = (int*) mmap(NULL, sizeof(int), PROT_READ|PROT_WRITE, MAP_SHARED, shd, 0);
+
+	if((cocinero = sem_open("/COCINERO", 0)) == SEM_FAILED){
+		printf("No se crearon los recursos compartidos: SEMAFORO COCINERO\n");
+		munmap(caldero, sizeof(int));
+		shm_unlink("/CALDERO");
+		exit(EXIT_FAILURE);
+	}
+	if((salvaje = sem_open("/SALVAJE", 0)) == SEM_FAILED){
+		printf("No se crearon los recursos compartidos: SEMAFORO SALVAJE\n");
+		munmap(caldero, sizeof(int));
+		shm_unlink("/CALDERO");
+		sem_close(cocinero);
+		exit(EXIT_FAILURE);
+	}
+
+	if((salvajes_q = sem_open("/SALVAJES_Q", 0)) == SEM_FAILED){
+		printf("No se crearon los recursos compartidos: SEMAFORO COCINERO\n");
+		munmap(caldero, sizeof(int));
+		shm_unlink("/CALDERO");
+		sem_close(cocinero);
+		sem_close(salvaje);
+		exit(EXIT_FAILURE);
+	}
+
+	savages(caldero);
+
+	munmap(caldero, sizeof(int));
+	sem_close(cocinero);
+	sem_close(salvaje);
+	sem_close(salvajes_q);
+	printf("SALVAJE %lu terminado\n", (unsigned long) getpid());
 	return 0;
 }
